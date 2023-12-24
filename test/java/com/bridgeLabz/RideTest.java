7422f97fea4b11255e44f83b998b303a4cbca670
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-12-24 12:39:19 +0530

    Added: unit test case and  calculateFare method

---
+++ test/java/com/bridgeLabz/RideTest.java
@@ -0,0 +1,21 @@
+package com.bridgeLabz;
+import com.bridgeLabz.service.RiderService;
+import com.bridgeLabz.service.entity.Rider;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+class RideTest {
+    // UC1
+    @Test
+    public void Km5_With_min5_should_return_totalFare_Rs15(){
+        Rider rider= new Rider(1,5.0,5.0);
+        Assertions.assertEquals(55.0, RiderService.calculateFare(rider.distance,rider.time));
+    }
+    @Test
+    public void hundredmetre_With_one_min_should_return_totalFare_Rs5(){
+        Rider rider= new Rider(1,0.1,1.0);
+        Assertions.assertEquals(5.0, RiderService.calculateFare(rider.distance,rider.time));
+
+    }
+
+
+}
