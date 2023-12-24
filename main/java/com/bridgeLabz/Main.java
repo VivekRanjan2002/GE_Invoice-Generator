b1321d73937f444dce6818ee625e502939c6fd6d
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-12-24 12:37:48 +0530

    Added: Welcome Message

---
+++ main/java/com/bridgeLabz/Main.java
@@ -0,0 +1,7 @@
+package com.bridgeLabz;
+public class Main {
+    public static void main(String[] args) {
+        //welcome message 
+        System.out.println("Welcome to Invoice Generator Program");
+    }
+}
